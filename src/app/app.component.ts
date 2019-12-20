46569ff56c7c435bb3d9d75053d9e76c7da19490
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2019-12-20 23:13:29 +0700

    Change currency to VND

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { CartService } from 'ng-shopping-cart';
+import ProductCartItem from './models/cart-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    private cartService: CartService<ProductCartItem>
+  ) {
+    this.cartService.setLocaleFormat("VND");
+  }
+}
